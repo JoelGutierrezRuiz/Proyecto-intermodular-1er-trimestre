21d4b774a3a23339532e858a91f50ae7a7cfe305
Joel <joelgutierrezruiz11@gmail.com>
2024-11-12 20:03:37 +0100

    Las categorias se pueden visualizar

--- Js/productos.js
+++ Js/productos.js
@@ -4,16 +4,56 @@ let url = "http://" + serverIp + "/Ludico/Api/ServicioProductos/controlador/prod
 
 const searchInput = document.getElementById("buscador");
 const searchButton = document.getElementById("lupa");
+const resultsContainer = document.getElementById("resultado-productos-row");
 
 
-searchButton.addEventListener("click",()=>{
+function createHtmlProduct(product) {
 
-    let search = "?nombre="+searchInput.value;
+    const container = document.createElement("div");
+    container.classList.add("producto","col-6","col-sm-4","col-xl-3");
 
-    
+    const redir = document.createElement("a");
+    redir.href = "../Controladores/detalle.php?nombre=" + product.nombre;
+
+    const img = document.createElement("img");
+    img.src = "../Imágenes/juegos/" + product.foto;
+
+    const name = document.createElement("p");
+    name.innerHTML = product.nombre;
+
+    const price = document.createElement("p");
+    price.innerHTML = product.precio
+
+    container.appendChild(redir);
+    container.appendChild(img);
+    container.appendChild(name);
+    container.appendChild(price);
+
+    return container;
+
+}
 
 
-    fetch(url+search).then((mes)=>mes.json())
-    .then((mes)=>console.log(mes))
-    .catch((err)=>console.log(err))
+function printAllProducts(products) {
+
+    console.log(products)
+
+    for (let i = 0; i < products.length; i++) {
+        resultsContainer.appendChild(createHtmlProduct(products[i]));
+    }
+    for (let i = 0; i < products.length; i++) {
+        resultsContainer.appendChild(createHtmlProduct(products[i]));
+    }
+}
+
+
+searchButton.addEventListener("click", () => {
+
+    let search = "?categoria=Familiar" //+ searchInput.value;
+
+
+    fetch(url + search).then((mes) => mes.json())
+        .then((mes) => printAllProducts(mes))
+        .catch((err) => console.log(err))
+
 })
