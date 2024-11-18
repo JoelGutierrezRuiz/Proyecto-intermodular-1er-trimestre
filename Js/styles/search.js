7db5e8b4b6a3094f7d4b89c04e8357a06d423be3
joel <joelgutierrezruiz11@gmail.com>
2024-11-18 03:17:13 +0100

    Header acabado definitivamente

--- Js/styles/search.js
+++ Js/styles/search.js
@@ -1,7 +1,3 @@
-//Esto es mas bien funcionalidad del headear
-
-
-
 const searchButton = document.getElementById("lupa");
 const searchInput = document.getElementById("buscador");
 
@@ -13,24 +9,42 @@ searchButton.addEventListener("click", () => {
 
 const popUpSearchContainer = document.getElementById("buscador-emergente-contenedor")
 const popUpSearchButton = document.getElementById("lupa-emergente");
+
+
 const popSearchInput = document.getElementById("buscador-input-emergente");
 const openSearchPopUp= document.getElementById("abrir-buscador-emergente");
 popUpSearchButton.addEventListener("click", () => {
     window.location.href = "http://localhost/Ludico/Vistas/productos.html?nombre=" + popSearchInput.value;
 })
 openSearchPopUp.addEventListener("click",()=>{
-    if (popUpSearchContainer.classList.contains('d-none')) {
-        popUpSearchContainer.classList.remove('d-none');
-    } else {
-        popUpSearchContainer.classList.add('d-none');
+    if(popUpSearchContainer.classList.contains("d-block")) {
+        popUpSearchContainer.classList.add("d-none");
+        popUpSearchContainer.classList.remove("d-block");
+        popUpSearchContainer.classList.remove("d-md-none");
+    }
+    else{
+        popUpSearchContainer.classList.add("d-block");
+        popUpSearchContainer.classList.add("d-md-none");
+        popUpSearchContainer.classList.remove("d-none");
+        
     }
 })
 
+const popUpCat = document.getElementById("boton-categorias");
+const catList = document.getElementById("lista-categorias");
+
+popUpCat.addEventListener("click",()=>{
+
+    if(catList.classList.contains("d-xl-block")) {
+        catList.classList.remove("d-xl-block");
+    }
+    else{
+        catList.classList.add("d-xl-block");
+    }
+
+});
+
 const options = document.getElementById("categorias");
-
-
-
-
 
 
 optionClicables()
@@ -54,12 +68,13 @@ document.getElementById('menu').addEventListener('click', () => {
     const options = document.getElementById('opciones-emergente');
     if (options.classList.contains('d-none')) {
         options.classList.remove('d-none');
+        options.classList.add('d-xl-none');
     } else {
         options.classList.add('d-none');
+        options.classList.remove('d-xl-none');
     }
 });
-document.getElementById('closeOptions').addEventListener("click",
-    function () {
+document.getElementById('closeOptions').addEventListener("click",()=>{
         const options = document.getElementById('opciones-emergente');
         if (options.classList.contains('d-none')) {
             options.classList.remove('d-none');
